a72717da14ef147a97c95d66cbb4882487dc5b66
Alexanderdp17 <104165148+Alexanderdp17@users.noreply.github.com>
2022-05-23 21:46:09 -0500

    Add files via upload

---
+++ ejercicio05/Main.java
@@ -0,0 +1,13 @@
+package ejercicio05;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Usuario usuario = new Usuario();
+
+        System.out.println(usuario.nombre);
+        System.out.println(usuario.email);
+        System.out.println(usuario.password);
+    }
+}
---
+++ ejercicio05/Usuario.java
@@ -0,0 +1,18 @@
+package ejercicio05;
+
+public class Usuario
+{
+    String nombre = "Alexander";
+
+    String email = "alexander@ale.com";
+
+    String password = "123456";
+
+    public static void main(String[] args)
+    {
+        Usuario usuario = new Usuario();
+
+        System.out.println(usuario.nombre);
+
+    }
+}
